59e09f51decd0ed80b90b361016e6e5595693b66
HigherGround189 <thehigherground18@gmail.com>
2025-11-02 01:28:58 +0800

    removed complex_quant_script for being too complex for github to handle

--- complex_quant_script.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-hello = YOLO("./models/yolo11n.pt")
-hello.export(format="onnx")
--- debug.py
+++
@@ -1,5 +0,0 @@
-import torch
-import torchvision
-print(torch.cuda.is_available())
-print(torch.__version__)
-print(torchvision.__version__)
