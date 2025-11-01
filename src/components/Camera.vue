0a0a6afc8525f34bee8729e3da51ecb550ca0d4f
HigherGround189 <thehigherground18@gmail.com>
2025-11-02 01:25:23 +0800

    slightly changed text to test github actions

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,7 +1,7 @@
 !<template>
   <div class="webcam-wrapper">
-    <img src="https://images.unsplash.com/photo-1761872936185-4ece7c1128ab?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170" alt="" id="webcam"/>
-    <p>Video Stream unavailable <br/>:(</p>
+    <img src="" alt="" id="webcam"/>
+    <p>Video Stream Loading...</p>
   </div>
 </template>
 
